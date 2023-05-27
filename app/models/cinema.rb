06a28761438fed5c99cf5bc408cc40cc033db554
camoanh <nguyencamoanh013@gmail.com>
2023-05-27 14:38:37 +0700

    Update 2

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -1,4 +1,3 @@
 class Cinema < ApplicationRecord
-    belongs_to :movie
     validates_presence_of :Room, :Row, :Number
 end
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,4 +1,3 @@
 class Producer < ApplicationRecord
-    belongs_to :movie
     validates_presence_of :Director, :Actor, :Writer
 end
